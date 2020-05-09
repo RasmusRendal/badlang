15a8db63f2df09d1d4645633bcd8d098efa7db50
Rasmus Rendal <rasmus@rend.al>
2020-05-09 10:12:46 +0200

    Implemented procedure calls

--- src/generator.py
+++ src/generator.py
@@ -67,6 +67,17 @@ class SymbolManager():
         pos = self.currentPos - symbol.position - symbol.size
         return str(pos) + "(%rsp)"
 
+    def PopStack(self):
+        code = ""
+        for s in self.symbols:
+            code += "pop" + AssemblySuffix(self.symbols[s].size) + " %rcx\n"
+        self.symbols = {}
+        self.currentPos = 0
+        self.anonNameIndex = 0
+        return code
+
+
+
 # From an expr, we want to return
 # a symbol
 class generator(PTNodeVisitor):
@@ -75,10 +86,8 @@ class generator(PTNodeVisitor):
         super().__init__()
         self.symbolManager = SymbolManager()
         self.literalManager = literalManager()
-        self.code = ".global _start\n"
-        self.code += "\n"
-        self.code += ".text\n"
-        self.code += "_start:\n"
+        self.procedures = []
+        self.code = ""
 
     # Returns address of string literal, and size
     def visit_string_literal(self, node, children):
@@ -95,9 +104,6 @@ class generator(PTNodeVisitor):
         varName = str(node)
         symbol = self.symbolManager.symbols[varName]
         return symbol
-
-    def visit_addition(self, node, children):
-        print(children)
 
     def visit_expr(self, node, children):
         val = children[0]
@@ -133,19 +139,62 @@ class generator(PTNodeVisitor):
         self.code += "syscall\n"
 
 
-    def visit_functionCall(self, node, children):
+    def visit_procedureCall(self, node, children):
         arg = children[1]
-        function = children[0]
-        if function == "print":
+        procedure = children[0]
+        if procedure == "print":
             return self.builtin_print(arg)
+        elif procedure in self.procedures:
+            self.code += "call" + AssemblySuffix(POINTER_SIZE) + " _" + procedure + "\n"
         else:
-            raise ValueError("Undefined function")
+            raise ValueError("Undefined procedure")
 
+    # This is all a horrible hack, but if it works?
     def visit_statement(self, node, children):
+        code = self.code
+        self.code = ""
+        return code
+
+    def visit_statements(self, node, children):
+        out = ""
+        for c in children:
+            out += c
+        return out
+
+    def visit_procedure(self, node, children):
+        procName = children[0]
+        code = children[1]
+        self.procedures.append(procName)
+        return "_" + procName + ":\n" + code + self.symbolManager.PopStack() + "ret" + AssemblySuffix(POINTER_SIZE) + "\n"
+
+    def visit_procedures(self, node, children):
+        out = ""
+        for c in children:
+            out += c
+        return out
+
+    def visit_program(self, node, children):
+        procedures = None
+        statements = None
+        if (len(children) == 2):
+            procedures = children[0]
+            statements = children[1]
+        else:
+            statements = children[0]
+        self.code = ".global _start\n"
+        self.code += "\n"
+        self.code += ".text\n"
+        if procedures != None:
+            self.code += procedures
+        self.code += "_start:\n"
+        self.code += statements
         self.code += "mov $60, %rax\n"
         self.code += "xor %rdi, %rdi\n"
         self.code += "syscall\n"
         self.code += self.literalManager.PrintLiteralPart()
+
+
+
 
 def code_generation(tree):
     gen = generator(debug=False)
--- src/parser.py
+++ src/parser.py
@@ -2,12 +2,16 @@ from arpeggio import Optional, ZeroOrMore, OneOrMore, EOF, ParserPython, PTNodeV
 from arpeggio import RegExMatch as _
 
 def name():               return _(r'[a-zA-Z]*')
-def string_literal():     return '"', _(r'[a-z A-Z]*'), '"'
+def string_literal():     return '"', _(r'[a-z A-Z, ?]*'), '"'
 def integer():            return _(r'[0-9]+')
 def var():                return name
 def expr():               return [integer, string_literal, var], ZeroOrMore("+", expr)
 def var_decl():           return name, name, "=", expr
-def functionCall():       return name, "(", expr, ")"
-def statement():          return OneOrMore([var_decl, functionCall]), EOF
+def procedureCall():      return name, "(", expr, ")"
+def statement():          return [var_decl, procedureCall]
+def statements():         return OneOrMore(statement)
+def procedure():          return "proc", name, "{", statements, "}"
+def procedures():         return OneOrMore(procedure)
+def program():            return Optional(procedures), statements, EOF
 
-parser = ParserPython(statement)
+parser = ParserPython(program)
